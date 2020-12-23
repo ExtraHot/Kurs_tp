c25d389891ae1c1bce0062fc570d8fa8a11a466f
ExtraHot <76525656+ExtraHot@users.noreply.github.com>
2020-12-23 18:36:58 +0300

    Создал головной файл с иноформацией машины

---
+++ Kurts_tp/Car.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+const std::string STATS[] = { "Модель", "Год","Цвет","Объём двигателя","АКП/МКП","Мощность двигателя","Топливный бак(объём в литрах в литрах)","Расход топлива на 100 км","Габариты","Подогрев руля","abs","eds","Система навигации","Бортовой компьютер","Подогрев сидений","Багажник на крышу","Подушки безопасности" };
+
+class car
+{
+	string info[17];
+public:
+	car();
+	car(ifstream& fin);
+	void print();
+	void save(ofstream& fout);
+	string getModel();
+};
