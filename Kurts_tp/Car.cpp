dc1cbc881679f7911834960a64de766ebceb7635
ExtraHot <76525656+ExtraHot@users.noreply.github.com>
2020-12-23 18:56:13 +0300

    Исправил ошибки

--- Kurts_tp/Car.cpp
+++ Kurts_tp/Car.cpp
@@ -1,17 +1,6 @@
 #include "car.h"
 
-car::car()
-{
-    cin.clear();
-    while (cin.get() != '\n');
-    for (size_t i = 0; i < 17; i++)
-    {
-        cout << STATS[i] << ": ";
-        getline(cin, info[i]);
-    }
-}
-
-car::car(ifstream& fin)
+car::car(ifstream& fin) // Файловый ввод
 {
     for (int i = 0; i < 17; i++)
     {
@@ -19,22 +8,34 @@ car::car(ifstream& fin)
     }
 }
 
-void car::print()
-{
-    for (size_t i = 0; i < 17; i++)
-    {
-        cout << STATS[i] << ": " << info[i] << std::endl;
-    }
-    cout << endl;
-}
-
-void car::save(ofstream& fout)
+void car::save(ofstream& fout)// Файловый вывод
 {
     for (size_t i = 0; i < 17; i++)
     {
         fout << info[i] << endl;
     }
 }
+
+car::car()
+{
+    cin.clear();
+    while (cin.get() != '\n');
+    for (size_t i = 0; i < 17; i++)// Ввод со строки
+    {
+        cout << STATS[i] << ": ";
+        getline(cin, info[i]);
+    }
+}
+
+void car::print()
+{
+    for (size_t i = 0; i < 17; i++)// Вывод в строку
+    {
+        cout << STATS[i] << ": " << info[i] << std::endl;
+    }
+    cout << endl;
+}
+
 
 string car::getModel()
 {
