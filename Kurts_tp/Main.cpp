fe74292df2ec9b390053418dedff8e2d8168cdc8
ExtraHot <76525656+ExtraHot@users.noreply.github.com>
2020-12-24 21:00:31 +0300

    Начало работы с main.cpp

---
+++ Kurts_tp/Main.cpp
@@ -0,0 +1,20 @@
+#include "handler.h"
+
+int main(int argc, char const* argv[])
+{
+    setlocale(LC_ALL, "Rus");
+    system("chcp 1251");
+
+    try 
+    {
+
+
+
+
+
+    }
+    catch (string err)
+    {
+        cout << err << endl;
+    }
+}
