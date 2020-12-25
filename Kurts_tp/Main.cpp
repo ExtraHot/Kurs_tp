7e35f47aa6770b677de6552f53178eb249c78974
ExtraHot <76525656+ExtraHot@users.noreply.github.com>
2020-12-25 15:28:41 +0300

    Переменные для работы с машинами

--- Kurts_tp/Main.cpp
+++ Kurts_tp/Main.cpp
@@ -5,9 +5,22 @@ int main(int argc, char const* argv[])
     setlocale(LC_ALL, "Rus");
     system("chcp 1251");
 
+   
+
     try 
     {
+        handler* salon = new handler("salon");
+        salon->set_next(new handler("dealer"));
+        salon->getNextPtr()->set_next(new handler("fabrique"));
+        handler* ptr = nullptr;
+        int input;
+        string model;
+        while (1)
+        {
 
+
+
+        }
 
 
 
